05f38d6ddb9021bad9ef1f656041a56d98b1f0c8
DevisBushi <dbushi@outlook.com>
2021-09-27 15:55:04 +0200

    moved api helper classes to UI.library completet cycle for authentication and token usage

--- TRMDesktopUI.Library/Api/APIHelper.cs
+++ TRMDesktopUI.Library/Api/APIHelper.cs
@@ -3,22 +3,23 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Text;
 using System.Threading.Tasks;
+using TRMDesktopUI.Library.Models;
 using TRMDesktopUI.Models;
 
-namespace TRMDesktopUI.Helpers
+namespace TRMDesktopUI.Library.Api
 {
-    public interface IAPIHelper
-    {
-        Task<AuthenticatedUser> Authenticate(string username, string password);
-    }
+
 
     public class APIHelper : IAPIHelper
     {
+        private readonly ILoggedInUserModel _loggedInUser;
         private HttpClient apiClient;
 
-        public APIHelper()
+        public APIHelper(ILoggedInUserModel loggedInUser)
         {
+            _loggedInUser = loggedInUser;
             InitializeClient();
         }
 
@@ -52,6 +53,32 @@ namespace TRMDesktopUI.Helpers
                     throw new Exception(response.ReasonPhrase);
                 }
 
+            }
+        }
+
+        public async Task GetLoggedInUserInfo(string token)
+        {
+            apiClient.DefaultRequestHeaders.Clear();
+            apiClient.DefaultRequestHeaders.Accept.Clear();
+            apiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            apiClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
+
+            using (HttpResponseMessage response = await apiClient.GetAsync("/api/User"))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadAsAsync<LoggedInUserModel>();
+                    _loggedInUser.Id = result.Id;
+                    _loggedInUser.CreatedDate = result.CreatedDate;
+                    _loggedInUser.FirstName = result.FirstName;
+                    _loggedInUser.LastName = result.LastName;
+                    _loggedInUser.EmailAddress = result.EmailAddress;
+                    _loggedInUser.Token = token;
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
             }
         }
     }
--- TRMDesktopUI/ViewModels/LoginViewModel.cs
+++ TRMDesktopUI/ViewModels/LoginViewModel.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using Caliburn.Micro;
 using TRMDesktopUI.Helpers;
+using TRMDesktopUI.Library.Api;
 
 namespace TRMDesktopUI.ViewModels
 {
@@ -91,6 +92,10 @@ namespace TRMDesktopUI.ViewModels
             {
                 ErrorMessage = "";
                 var result = await _apiHelper.Authenticate(UserName, Password);
+
+                // capture more info about the user.
+
+                await _apiHelper.GetLoggedInUserInfo(result.Access_Token);
             }
             catch (Exception ex)
             {
