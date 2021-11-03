62cf9bfad087993e8dbe4daaa75622affb2f403a
DevisBushi <dbushi@outlook.com>
2021-11-03 16:19:52 +0100

    improvements in data access tier classes

--- TRMDataManager.Library/Internal/DataAccess/SqlDataAccess.cs
+++ TRMDataManager.Library/Internal/DataAccess/SqlDataAccess.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
@@ -7,7 +8,7 @@ using Dapper;
 
 namespace TRMDataManager.Library.Internal.DataAccess
 {
-    internal class SqlDataAccess
+    internal class SqlDataAccess : IDisposable
     {
         public string GetConnectionString(string name)
         {
@@ -34,5 +35,50 @@ namespace TRMDataManager.Library.Internal.DataAccess
 
             }
         }
+
+        private IDbConnection _connection;
+
+        private IDbTransaction _transaction;
+        // Open connect/start transaction method
+        public void StartTransaction(string connectionStringName)
+        {
+            string connectionString = GetConnectionString(connectionStringName);
+            _connection = new SqlConnection(connectionString);
+            _connection.Open();
+            _transaction = _connection.BeginTransaction();
+        }
+
+        public void CommitTransaction()
+        {
+            _transaction?.Commit();
+            _connection?.Close();
+        }
+
+        public void RollbackTransaction()
+        {
+            _transaction?.Rollback();
+            _connection?.Close();
+        }
+
+        public void Dispose()
+        {
+            CommitTransaction();
+        }
+        // load using the transaction
+        public List<T> LoadDataInTransaction<T, U>(string storedProcedure, U parameters)
+        {
+            List<T> rows = _connection.Query<T>(storedProcedure, parameters, commandType: CommandType.StoredProcedure, transaction: _transaction).ToList();
+
+                return rows;
+            
+        }
+        // Save using the transaction
+        public void SaveDataInTransaction<T>(string storedProcedure, T parameters)
+        {
+            _connection.Execute(storedProcedure, parameters, commandType: CommandType.StoredProcedure, transaction: _transaction);
+        }
+        // Close connection/stop transaction method
+        // Dispose
+
     }
 }
