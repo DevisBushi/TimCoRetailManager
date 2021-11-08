5d88524b9b6e6313b6df5c1ff4c39d719f72cf24
devisbushi <dbushi@outlook.com>
2021-11-09 00:15:56 +0100

    changed API to .net core 3.1

---
+++ TRMApi/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using TRMApi.Models;
+
+namespace TRMApi.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public HomeController(
+            ILogger<HomeController> logger,
+            UserManager<IdentityUser> userManager,
+            RoleManager<IdentityRole> roleManager)
+        {
+            _logger = logger;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Privacy()
+        {
+            //string[] roles = {"Admin", "Manager", "Cashier"};
+
+            //foreach (var role in roles)
+            //{
+            //    var roleExists = await _roleManager.RoleExistsAsync(role);
+            //    if (roleExists == false)
+            //    {
+            //        await _roleManager.CreateAsync(new IdentityRole(role));
+            //    }
+            //}
+
+            //var user = await _userManager.FindByEmailAsync("admin@test.com");
+
+            //if (user != null)
+            //{
+            //    await _userManager.AddToRoleAsync(user, "Admin");
+            //    await _userManager.AddToRoleAsync(user, "Cashier");
+            //}
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ TRMApi/Controllers/InventoryController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using TRMDataManager.Library.DataAccess;
+using TRMDataManager.Library.Models;
+
+namespace TRMApi.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InventoryController : ControllerBase
+    {
+        [Authorize(Roles = "Manager,Admin")]
+        public List<InventoryModel> Get()
+        {
+            InventoryData data = new InventoryData();
+            return data.GetInentory();
+        }
+        [Authorize(Roles = "Admin")]
+        public void Post(InventoryModel item)
+        {
+            InventoryData data = new InventoryData();
+            data.SaveInventoryRecord(item);
+        }
+    }
+}
